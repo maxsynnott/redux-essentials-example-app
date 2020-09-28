1222d7632d2450bb6dd18a217ba8c5e132658fd1
maxsynnott <maxryansynnott@gmail.com>
2020-09-28 14:26:54 +0200

    AddPostForm in and functioning

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
     <nav>
       <section>
@@ -13,3 +13,5 @@ export const Navbar = () => {
     </nav>
   )
 }
+
+export default Navbar
